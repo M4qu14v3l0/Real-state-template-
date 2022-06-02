a2fae2b6c8953f230248cf9267e0b736a49d4ca2
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2022-06-01 21:21:29 -0500

    1 page left

---
+++ js/text.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll('.general-wrapper');
+
+const options = {
+    root: null,
+    threshold: 0.3,
+    rootMargin: '50px',
+}
+
+const observer = new IntersectionObserver(function(entries , observer){
+    entries.forEach(entry => {
+        if(!entry.isIntersecting){
+            return;
+        }
+        entry.target.classList.toggle('show-wrappers');
+        observer.unobserve(entry.target);
+    });
+
+},options);
+
+sections.forEach(section => {
+    observer.observe(section);
+});
